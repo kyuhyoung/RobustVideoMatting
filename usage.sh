289e03a379354245483b546c0924379f821fc023
Kyuhyoung Choi <khchoi@fxgear.net>
2023-04-11 13:47:16 +0900

    Minor.

--- usage.sh
+++ usage.sh
@@ -1,22 +1,26 @@
 #   ceil_mode should be false sicne current Barracuda does NOT support AvgPool2D(ceil_mode = True)
 ceil_mode=false
 #for model in mobilenetv3
-for model in resnet50
+#for model in resnet50
+for model in resnet50 mobilenetv3
 do
     #for checkpoint in rvm_mobilenetv3.pth
     for checkpoint in rvm_${model}
     do
-        for wh in 2560_1440 1440_2560 1920_1080 1080_1920
+        #for wh in 2560_1440 1440_2560 1920_1080 1080_1920
         #for wh in 1920_1080 1280_720
         #for wh in 1920_1080
         #for wh in 1920_1080 960_540
         #for wh in 960_540
         #for wh in 480_270
         #for wh in 1080_1920
+        for wh in 2160_3840
         #for wh in 1920_1088 1088_1920
         #for wh in 1920_1088 1088_1920 1080_1920
         #for wh in 1080_1920
         #for wh in 720_1280
+        #for wh in 6000_4000 4000_6000
+        #for wh in 6000_4000
         do
             for downsample_ratio in 1 0.5 0.25 0.125
             #for downsample_ratio in 1 0.25
@@ -24,6 +28,7 @@ do
             #for downsample_ratio in 1
             #for downsample_ratio in 0.25 0.125
             #for downsample_ratio in 0.25 0.5
+            #for downsample_ratio in 0.3 0.35 0.4
             #for downsample_ratio in 0.125
             do
                 #for precision in float32 float16
